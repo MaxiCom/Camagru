e8d3200de8c64556f92a59f3d2b6991deb016d56
Max <maximemorlet@hotmail.fr>
2016-01-27 01:56:44 +0100

    ok

--- javascript/login.js
+++ javascript/login.js
@@ -109,10 +109,9 @@ function form_login(form)
 function form_register(form)
 {
 	var request = "username=" + form.username.value
-				+ "&password=" + form.password.value;
+				+ "&password=" + form.password.value
 				+ "&email=" + form.email.value;
 	
-	alert(request);
 	ajax(request, "/includes/script/register.php", form);
 	return (false);
 }
